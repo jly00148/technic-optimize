6678637ebc9ab013d874c8df0de27b622162f6b7
jiangliaoyuan <453825818@qq.com>
2021-09-30 09:34:22 +0800

    iterate-obj

---
+++ 01-array/07-iterate-obj.js
@@ -0,0 +1,11 @@
+var obj = {name:'jly',age:18,sex:'man'}
+
+// 1.for ... in ...
+for(var i in obj){
+    console.log(i,obj[i])
+    // name jly
+    // age 18
+    // sex man
+}
+
+// 2.Object.keys() or Object.values()
