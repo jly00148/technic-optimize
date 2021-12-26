e3e98b00c4ee70e334ed2540406f6981d69795dd
jiangliaoyuan <453825818@qq.com>
2021-12-26 23:54:48 +0800

    shoal-copy

---
+++ 01-array-object/01-array/04-shoal-copy.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: jly
+ * @Date: 2021-12-26 01:15:32
+ * @LastEditTime: 2021-12-26 23:53:36
+ * @FilePath: \technic-optimize\01-array-object\01-array\04-shoal-copy.js
+ */
+
+var arr = [11,22,33]
+
+var arr1 = arr;
+
+console.log(arr === arr1);
