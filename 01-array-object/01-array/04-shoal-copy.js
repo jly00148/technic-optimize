307580b5eda2504f0d4e4eb982ce9c3197f2b41a
jiangliaoyuan <453825818@qq.com>
2021-12-27 17:09:48 +0800

    shoal-copy

--- 01-array-object/01-array/04-shoal-copy.js
+++ 01-array-object/01-array/04-shoal-copy.js
@@ -1,12 +1,19 @@
 /*
  * @Author: jly
  * @Date: 2021-12-26 01:15:32
- * @LastEditTime: 2021-12-26 23:53:36
+ * @LastEditTime: 2021-12-27 17:09:23
  * @FilePath: \technic-optimize\01-array-object\01-array\04-shoal-copy.js
  */
 
-var arr = [11,22,33]
 
+var arr = [11,22,33];
+
+// 浅copy一：
 var arr1 = arr;
 
-console.log(arr === arr1);
+// console.log(arr1 ===arr);
+// ------------------------------------------------------------------
+
+// 浅copy二：
+var arr2 = arr.slice()//不传参或者传参数0，默认从0开始可以复制数组。
+// console.log(arr === arr1);
