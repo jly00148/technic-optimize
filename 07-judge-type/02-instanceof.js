35965ca0ac5a8e030d8c88b58310a13651a9047f
jiangliaoyuan <453825818@qq.com>
2022-01-05 00:27:30 +0800

    instanceof

---
+++ 07-judge-type/02-instanceof.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: jly
+ * @Date: 2022-01-05 00:10:16
+ * @LastEditTime: 2022-01-05 00:25:52
+ * @FilePath: \technic-optimize\07-judge-type\02-instanceof.js
+ */
+
+// instanceof一般用于检测引用数据类型，A instanceof B，如果A是B的实例的话返回true否则返回false
+console.log('string' instanceof String);//false
+console.log(10 instanceof Number);//false
+console.log(true instanceof Boolean);//false
+console.log(null instanceof Object);//false
+console.log([11,22,33] instanceof Array);//true
+console.log({name:'jly'} instanceof Object);//true
+console.log(function fn(){} instanceof Function);//true
+console.log(new Date() instanceof Date);//true
+console.log(new Number() instanceof Number);//true
+console.log(new String() instanceof String);//true
+console.log(new Boolean() instanceof Boolean);//true
+console.log(/^123$/ instanceof RegExp);//true
