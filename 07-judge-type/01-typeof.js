b7c22854fe29cd9df86839f1cb8a8868ccdbdce4
jiangliaoyuan <453825818@qq.com>
2022-01-06 21:27:23 +0800

    typeof

--- 07-judge-type/01-typeof.js
+++ 07-judge-type/01-typeof.js
@@ -1,10 +1,11 @@
 /*
  * @Author: jly
  * @Date: 2022-01-04 17:54:54
- * @LastEditTime: 2022-01-05 00:17:02
+ * @LastEditTime: 2022-01-06 21:26:29
  * @FilePath: \technic-optimize\07-judge-type\01-typeof.js
  */
 
+// typeof用于判断基本数据类型
 console.log(typeof 'String');//string
 console.log(typeof 123456);//number
 console.log(typeof true);//boolean
@@ -21,3 +22,4 @@ console.log(typeof new Boolean(false));//object
 console.log(typeof String);//function
 console.log(typeof Array);//function
 console.log(typeof Boolean);//function
+console.log(typeof Number);//function
